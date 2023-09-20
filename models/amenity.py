57e8daa5751446605fa3a872aff62d86fdfda589
zakisu250 <zakisu250@gmail.com>
2023-09-20 11:23:34 +0300

    Amenity updated for db storage

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from models.base_model import Base
+from sqlalchemy import Column, Integer, Float, Strgin, ForeignKey, Table
+from sqlalchemy.orm import relationship
+import os
+import models.place import place_amenity
 
 class Amenity(BaseModel):
-    name = ""
+    """ Defines Amenity """
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place-amenities = realtionship('Place', secondary=place_amenity, back_populates='amenities')
+    else:
+        name = ""
