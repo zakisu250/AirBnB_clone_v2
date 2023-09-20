85f3b91cb6d7fd0e5d18b518bdcfa37adcab0e78
Zekaria Mohammed <36282172+zakisu250@users.noreply.github.com>
2023-09-20 07:49:51 -0700

    removed storage var assigned to None

--- models/__init__.py
+++ models/__init__.py
@@ -11,9 +11,6 @@ from models.place import Place
 from models.review import Review
 from os import getenv
 
-
-storage = None
-
 if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
