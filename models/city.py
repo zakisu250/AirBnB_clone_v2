b522eaa688cfb616c16c3ce79f61c27268468040
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-09-20 10:06:41 +0300

    Updating DBStorage User for #6

--- models/city.py
+++ models/city.py
@@ -9,3 +9,7 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey(states.id), nullable=False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
