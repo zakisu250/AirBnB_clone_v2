e18b06b39ce794b0b6f0d34d7312eb2a0c459337
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-09-20 12:07:25 +0300

    amenity last update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenities
 
 
 class Amenity(BaseModel):
-    name = ""
+    """Amenity inherits from BaseModel and Base"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenities)
+
