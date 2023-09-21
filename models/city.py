4069bd3600a79626a5545d3a20b5f2d68f5d821e
zakisu250 <zakisu250@gmail.com>
2023-09-21 09:56:12 +0300

    user, city and review pycodestyle fix

--- models/city.py
+++ models/city.py
@@ -9,5 +9,7 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan', backref="cities")
+    state_id = Column(String(60), ForeignKey("states.id"),
+                      nullable=False)
+    places = relationship("Place", cascade='all, delete,
+                          delete-orphan', backref="cities")
--- models/review.py
+++ models/review.py
@@ -8,5 +8,7 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable= False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'),
+                      nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'),
+                     nullable=False)
