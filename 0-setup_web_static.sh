c5fdc0c4e89cda8ad5a7c769c9134dec800d79b3
zakisu250 <zakisu250@gmail.com>
2023-10-05 21:43:15 +0300

    added the required line to line 11

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,10 +4,7 @@ apt-get update
 apt-get install -y nginx
 mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
 echo '<h1 style="text-align:center; padding-top:2rem;">This is it</h1>' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ndex.html /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "/error_page 404 \/page404.html;/a \\\n\tlocation /hbtn_static { \
-        \n\t\talias /data/web_static/current/; \
-        \n\t\tautoindex off; \
-        \n\t}" /etc/nginx/sites-available/default
+sed -i '11i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-enabled/default
 service nginx restart
