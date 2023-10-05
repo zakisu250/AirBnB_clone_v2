f195bf959e9f66b84016fd129283819343302094
zakisu250 <zakisu250@gmail.com>
2023-10-05 22:38:01 +0300

    Packing web_static directory

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """ Prepare the web_static directory by packing """
-from fabric.api import *
+from fabric.api import local
 import time
 
 
+#def do_pack():
+#    """ Packing the directory to create .tgz file """
+#    stime = time.strftime("%Y%m%d%H%M%S")
+#    local("mkdir -p versions")
+#    local("tar -cvzf versions/web_static_{}.tgz web_static/".format(stime))
+#    return ("versions/web_static_{}.tgz".format(stime))
 def do_pack():
-    """ Packing the directory to create .tgz file """
-    stime = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz webstatic_".
-              format(stime))
-        return ("versions/web_static_{}.tgz".format(stime))
-    except:
+    """
+        compresses web_static files into one file
+    """
+    local("mkdir -p versions")
+    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    tar_path = "versions/web_static_{}.tgz".format(current_date)
+    captured = local("tar -cvzf {} web_static".format(tar_path), capture=True)
+    if captured.failed:
         return None
+    return tar_path
