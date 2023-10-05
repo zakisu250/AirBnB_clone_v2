519ae4ffce65c3e7a1f3c9dff18b42d2d6d7b46b
zakisu250 <zakisu250@gmail.com>
2023-10-05 22:39:18 +0300

    removed commented code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,20 +4,10 @@ from fabric.api import local
 import time
 
 
-#def do_pack():
-#    """ Packing the directory to create .tgz file """
-#    stime = time.strftime("%Y%m%d%H%M%S")
-#    local("mkdir -p versions")
-#    local("tar -cvzf versions/web_static_{}.tgz web_static/".format(stime))
-#    return ("versions/web_static_{}.tgz".format(stime))
 def do_pack():
-    """
-        compresses web_static files into one file
-    """
+    """ Packing the directory to create .tgz file """
+    stime = time.strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
-    tar_path = "versions/web_static_{}.tgz".format(current_date)
-    captured = local("tar -cvzf {} web_static".format(tar_path), capture=True)
-    if captured.failed:
-        return None
-    return tar_path
+    local("tar -cvzf versions/web_static_{}.tgz web_static/".format(stime))
+    return ("versions/web_static_{}.tgz".format(stime))
+
