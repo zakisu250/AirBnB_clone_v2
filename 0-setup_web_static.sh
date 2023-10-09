de552b588c5d8f1e4f41bb10633701b28832d694
zakisu250 <zakisu250@gmail.com>
2023-10-09 09:50:03 +0300

    added new code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
-# Setup the nginx servers with directories to deploy web static
+# script that sets up your web servers for the deployment of web_static
 apt-get update
-apt-get install -y nginx
-mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-echo '<h1 style="text-align:center; padding-top:2rem;">This is it</h1>' > /data/web_static/releases/test/index.html
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Hellow World!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+
 chown -R ubuntu:ubuntu /data/
-sed -i '11i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-enabled/default
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root /var/www/html;
+    index index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://github.com/astuman/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+    }" > /etc/nginx/sites-available/default
 service nginx restart
