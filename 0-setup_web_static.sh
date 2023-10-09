ae335d22ecf167d9a52d492fbedb625a445a4d52
zakisu250 <zakisu250@gmail.com>
2023-10-09 11:26:34 +0300

    changed script to configure nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/env bash
-# Configires Nginx server with deploymeny folders
-apt-get update -y
-apt-get install -y nginx
-mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-echo "<marquee> Let's get it </marquee>" > /data/web_static/releases/test/index.html
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install nginx -y
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 chown -R ubuntu:ubuntu /data/
-# Write after the error declaration
-sed -i "/error_page 404 \/404.html;/a \\\n\tlocation /hbnb_static { \
-        \n\t\talias /data/web_static/current/; \
-        \n\t\tautoindex off; \
-        \n\t}" /etc/nginx/sites-available/default
-# restart nginx
+
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root /var/www/html;
+    index index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://zakisu.vercel.app;
+    }
+
+    error_page 404 /404.html;
+        location /404 {
+            root /var/www/html;
+            internal;
+        }
+    }" > /etc/nginx/sites-available/default
 service nginx restart
