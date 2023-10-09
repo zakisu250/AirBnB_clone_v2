460232b67c7b02c08075422ef612357a5c2af0d4
zakisu250 <zakisu250@gmail.com>
2023-10-09 09:53:47 +0300

    changed another script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,15 @@
 #!/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static
-apt-get update
-apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Hellow World!" > /data/web_static/releases/test/index.html
+# Configires Nginx server with deploymeny folders
+apt-get update -y
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+echo "<marquee> Let's get it </marquee>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-
 chown -R ubuntu:ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root /var/www/html;
-    index index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 https://github.com/astuman/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-        root /var/www/html;
-        internal;
-    }
-    }" > /etc/nginx/sites-available/default
+# Write after the error declaration
+sed -i "/error_page 404 \/404.html;/a \\\n\tlocation /hbnb_static { \
+        \n\t\talias /data/web_static/current/; \
+        \n\t\tautoindex off; \
+        \n\t}" /etc/nginx/sites-available/default
+# restart nginx
 service nginx restart
