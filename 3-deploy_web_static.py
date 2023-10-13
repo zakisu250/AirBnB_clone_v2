20637176beeb49019cbfb68d5ccdb124e0d2916b
zakisu250 <zakisu250@gmail.com>
2023-10-13 13:37:48 +0000

    refactored script

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -14,27 +14,28 @@ def do_pack():
     local("tar -cvzf versions/web_static_{}.tgz web_static/".format(stime))
     return ("versions/web_static_{}.tgz".format(stime))
 
+
 def do_deploy(archive_path):
-    """ distributes an archive to the web servers """
+    """ distributes an archive to my web servers """
     if exists(archive_path) is False:
         return False
-    filename = archive_path.split('/')[-1]
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    tmp = "/tmp/" + filename
-
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(no_tgz))
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         print("New version deployed!")
         return True
     except:
         return False
+
 
 def deploy():
     """ Create and deploy the web_static archive """
