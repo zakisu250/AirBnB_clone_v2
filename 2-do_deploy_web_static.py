fe5097901de6e3fdacb9eae2ede74fa90da63c1c
zakisu250 <zakisu250@gmail.com>
2023-10-13 13:22:55 +0000

    updated script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@
 """
 
 from fabric.api import *
-import os
+from os.path import exists
 
 env.hosts = ['54.174.136.120', '34.207.188.128']
 
@@ -17,20 +17,19 @@ def do_deploy(archive_path):
     """ distributes an archive to my web servers """
     if exists(archive_path) is False:
         return False
-    filename = archive_path.split('/')[-1]
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    tmp = "/tmp/" + filename
-
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(no_tgz))
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
-        print("New version deployed!")        
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        print("New version deployed!")
         return True
     except:
         return False
