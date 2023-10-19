b714c2998213d8afe924652743bd5a24c86a1679
zakisu250 <zakisu250@gmail.com>
2023-10-19 18:02:44 +0000

    hbnb route definition

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script starts a server at 0.0.0.0 on port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
