3164429b822a9eeb64ecd4b6c08e8f0c29d72f7a
zakisu250 <zakisu250@gmail.com>
2023-10-20 10:15:37 +0300

    display C is and a text input

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script starts a server at 0.0.0.0 on port 5000
+also accept character from the url
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Returns string Hello """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns a simple string for the route """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """ Returns a statement taken as text input """
+    return "C " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
