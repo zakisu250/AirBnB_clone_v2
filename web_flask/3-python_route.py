93aa521803328a42fd09ffdd4c6d7ff71d61f018
zakisu250 <zakisu250@gmail.com>
2023-10-20 10:18:26 +0300

    Python is cool display

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This script starts a server at 0.0.0.0 on port 5000
+also accept character from the url
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Returns string Hello """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns a simple string for the route """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """ Returns a statement taken as text input """
+    return "C " + text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='iscool'):
+    """ Display python is cool as default """
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
