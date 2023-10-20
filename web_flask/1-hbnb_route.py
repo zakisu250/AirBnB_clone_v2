aca1a1d7010974666df16209228c39272839c89d
zakisu250 <zakisu250@gmail.com>
2023-10-20 10:10:27 +0300

    fixing pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,15 +6,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ Returns string Hello """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Returns a simple string for the route """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
