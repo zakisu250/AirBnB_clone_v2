f25b0a39efaf03b5d372d1e1fcdebacc30bc968c
zakisu250 <zakisu250@gmail.com>
2023-10-20 20:34:13 +0300

    changed the imports

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,8 +12,10 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ Display the templates of states in ascending order """
-    states = sorted(list(storage.all("State").values()), key=lambda x:x.name)
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State")
+    dict_repr = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html',
+                            Table="States", states=dict_repr)
 
 @app.teardown_appcontext
 def teardown_db(exception):
