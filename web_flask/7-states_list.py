d94ef337ce24be2b48b9d62b4b63699024a70788
zakisu250 <zakisu250@gmail.com>
2023-10-20 16:53:11 +0300

    updated to list the places

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Displays a states list
+"""
+from flask import Flask, render_template
+from models import *
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Display the templates of states in ascending order """
+    states = sorted(list(storage.all("State").values()), key=lambda x:x.name)
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Close the session on teardown """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
