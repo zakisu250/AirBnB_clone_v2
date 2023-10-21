7cb4571c595437340afc81af3e05bc411d0d6d89
zakisu250 <zakisu250@gmail.com>
2023-10-21 10:03:47 +0300

    sorted the result in the html doc

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,17 +5,14 @@ Displays a states list
 from flask import Flask, render_template
 from models import *
 from models import storage
-
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ Display the templates of states in ascending order """
-    states = storage.all("State")
-    dict_repr = {value.id: value.name for value in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States", states=dict_repr)
+    states = storage.all("State").values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
