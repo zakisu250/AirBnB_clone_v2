59b91f272c9f17d45fb119d50a25b2a862b3c578
zakisu250 <zakisu250@gmail.com>
2023-10-22 10:00:11 +0300

    used set to set the state_id variable

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,23 +10,13 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    """ Display the templates of states in ascending order """
-    states = storage.all(State).values()
-    return render_template('9-states.html', Table="States",
-                           states=states, condition="states_list")
-
-
 @app.route('/states/<id>', strict_slashes=False)
-def states_with_id(id):
+def states_with_id(id=None):
     """ Display a detail on a selected state """
-    all_states = storage.all(State).values()
-    try:
-        state_ids = all_states[id]
-        return render_template('9-states.html', state_id=state_ids,
-                               condition="state_id")
-    except Exception:
-        return render_template("9-states.html", condition="not_found")
+    states = storage.all(State)
+    if id is not None:
+        id = 'State.' + id
+    return render_template('9-states.html', states=states, state_id=id)
 
 
 @app.teardown_appcontext
